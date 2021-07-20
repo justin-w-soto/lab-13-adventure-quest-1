3e6633a5e5349eaae3f778908cb9af6b478e3a0e
Justin W Soto <soto@pdx.edu>
2021-07-20 12:55:49 -0700

    added info for Q1 filth-pit

--- data/quest-data.js
+++ data/quest-data.js
@@ -2,43 +2,49 @@ const filthPit = {
     id: 'filth-pit',
     title: 'Imps in a Filth Pit',
     image: 'imp-in-filthpit.jpg',
-    description: `
-        
-    `,
+    description: `You have entered the lair of the Imps. A large pit filled with 
+    the refuse of a thousand kingdoms. It is disgusting, the air smells of fish and cheese. 
+    The Imps are sweaty and mean. They look like they work-out and they definitely
+    want to kick your ass. What are you going to do?`,
+
     choices: [{
         id: 'trick',
         description: 'Try to con them',
-        result: `
-            
-        `,
-        hp: 0,
+        result: `Imps cannot be conned by a simpleton such as yourself. 
+        The imps take a finger from your right hand as a reminder of your foolishness.
+        You find a bag of gold on your way out of the filth pit so it's not all bad, right?`,
+
+        hp: -10,
         gold: 35
     }, {
         id: 'fight',
         description: 'Attack!',
-        result: `
-     
-        `,
+        result: `You draw your weapon, but the imps strike first. 
+        They cut off both of your arms in the battle, but you use 
+        some magic beans you found to poison them all. You run out of the filth pit 
+        without arms but you have stolen all of the impish gold from the pit.`,
+
         hp: -30,
-        gold: 50
+        gold: 150
     }, {
         id: 'run',
         description: 'Retreat!!',
-        result: `
-            
-        `,
-        hp: -50,
+        result: `You turn to run, but slip on a discarded human face that the imps 
+        have left behind from their last feast. In a rare moment of genius, 
+        you use the face to disguise yourself and leave the filth pit unnoticed.`,
+
+        hp: 35,
         gold: 0
     }]
 };
 
 const mermaid = {
     id: 'mermaid',
-    title: 'An evil mermaid zombie!!',
+    title: 'An evil Fish lady!!',
     image: 'mermaid.jpg',
     
     description: `
-        
+    
     `,
     choices: [{
         id: 'run',
@@ -70,7 +76,7 @@ const mermaid = {
 const unknown = {
     id: 'unknown',
     title: 'unknown',
-    prerequisites: ['filth-pit', 'moermaid'],
+    prerequisites: ['filth-pit', 'mermaid'],
     image: '',
    
     description: `
